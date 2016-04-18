c49ad5dc53c7af416f826ae3ebaf21369df11524
Steven Sun <stevesunmkii@gmail.com>
2016-04-17 21:25:08 -0400

    correct

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 var port = (process.env.VCAP_APP_PORT || 3000);
 var host = (process.env.VCAP_APP_HOST || 'localhost');
-connect().use(serveStatic(__dirname)).listen(port,host);
+connect().use(serveStatic(__dirname+ '/public')).listen(port,host);
