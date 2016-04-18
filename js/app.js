4d1aeb2996ef8ba541c90a05477aceb3008f5feb
Steven Sun <stevesunmkii@gmail.com>
2016-04-17 21:34:09 -0400

    test

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
-var doplay = angular.module('dotplay', []);
-
-    // create the controller and inject Angular's $scope
-    doplay.controller('mainController', function($scope) {
-
-        // create a message to display in our view
-        $scope.message = 'Everyone come and see how good I look!';
-    });
+var appname = angular.module('appname', []);
+appname.controller('appCtrl', ['$scope',
+  function($scope) {
+    $scope.greeting = { text: 'Hello' };
+}]);
