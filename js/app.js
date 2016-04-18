7131066d4f825d4a9d9fbfa77f4bbc872dbe6bc0
Steven Sun <stevesunmkii@gmail.com>
2016-04-17 21:07:24 -0400

    test

--- js/app.js
+++ js/app.js
@@ -1,30 +1,8 @@
-<!-- index.html -->
-    <!DOCTYPE html>
+var scotchApp = angular.module('scotchApp', []);
 
-    <!-- define angular app -->
-    <html ng-app="scotchApp">
-    <head>
-      <!-- SCROLLS -->
-      <!-- load bootstrap and fontawesome via CDN -->
-      <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" />
-      <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.0/css/font-awesome.css" />
+    // create the controller and inject Angular's $scope
+    scotchApp.controller('mainController', function($scope) {
 
-      <!-- SPELLS -->
-      <!-- load angular via CDN -->
-      <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular.min.js"></script>
-          <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular-route.js"></script>
-      <script src="script.js"></script>
-    </head>
-
-    <!-- define angular controller -->
-    <body ng-controller="mainController">
-
-    ...
-
-    <!-- MAIN CONTENT AND INJECTED VIEWS -->
-    <div id="main">
-        {{ message }}
-
-        <!-- angular templating -->
-        <!-- this is where content will be injected -->
-    </div>
+        // create a message to display in our view
+        $scope.message = 'Everyone come and see how good I look!';
+    });
