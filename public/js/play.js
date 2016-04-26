7cfda2e315bd784b42d1bd1ade44ed8566d0b06f
William A. Foster <fosterw@purdue.edu>
2016-04-25 20:51:07 -0400

    Game field
    Added the field in witch the game will be played

--- public/js/play.js
+++ public/js/play.js
@@ -10,4 +10,24 @@ angular.module('dotplay')
     toastr.info('This is a test message.');
   };
 
+  $scope.setupDots = function (max_rows, max_collumns) {
+		var collumn, row;
+		var stage = new createjs.Stage("myCanvas");
+		for(collumn = 1; collumn < max_collumns + 1; collumn++){
+			for(row = 1; row < max_rows + 1; row++) {
+				var circle = new createjs.Shape();
+				circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 10);
+				circle.x = 100 * row;
+				circle.y = 100 * collumn;
+				stage.addChild(circle);
+				stage.update();
+			}
+		}
+	};
+  
+  $scope.setupDots(5,5);
+  
 });
+
+
+//scope.setupDots(5,5);
