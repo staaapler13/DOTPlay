f9cac4e8cf7684a1ea03f930e26010b63fc769e2
Steven Sun <stevesunmkii@gmail.com>
2016-04-25 20:36:08 -0400

    Revert "socket.io test"
    This reverts commit 57139f2ad0d0f96adf3d1468a7b1ab5313f7d52d.

--- server.js
+++ server.js
@@ -7,13 +7,23 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
+
+// create a new express server
 var app = express();
+
+// serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-io.on('connection', function(){
 
-  console.log('User connected');
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
 
+// start server on the specified port and binding host
+app.listen(appEnv.port, '0.0.0.0', function() {
+
+	// print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
 });
-server.listen(3000);
