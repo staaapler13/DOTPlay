8a61b03c2ec97b466daf2af51c5362ffc7570391
William A. Foster <fosterw@purdue.edu>
2016-04-26 01:56:01 -0400

    Game Lines Responding
    Lines in Game Field now respond to being clicked by lighting up

--- public/js/play.js
+++ public/js/play.js
@@ -26,18 +26,32 @@ angular.module('dotplay')
 				// draw horiontal lines
 				if(collumn > 1) {
 					var line = new createjs.Shape();
+					line.name = "vertical_line"+row+"x"+(collumn-1);
 					line.graphics.beginFill("DeepSkyBlue").drawRect(0,0,10,-(750/max_rows));
 					line.x = 20 + (750/max_rows) * row;
 					line.y = 25 + (750/max_collumns) * collumn;
+					line.alpha = 0.05;
 					stage.addChild(line);
+					//line.graphics.beginFill("FireBrick").drawRect(0,0,10,-(750/max_rows));
+					line.on("click", function(evt) {
+						//alert("type: "+evt.type+" target: "+evt.target);
+						this.alpha = 1;
+						stage.update();
+					});
 				}
 				// draw verticle lines
 				if(row > 1) {
 					var line = new createjs.Shape();
+					line.name = "horiontal_line"+(row-1)+"x"+collumn;
 					line.graphics.beginFill("DeepSkyBlue").drawRect(0,0,-(750/max_collumns),10);
 					line.x = 25 + (750/max_rows) * row;
 					line.y = 20 + (750/max_collumns) * collumn;
+					line.alpha = 0.05;
 					stage.addChild(line);
+					line.on("click", function(evt) {
+						this.alpha = 1;
+						stage.update();
+					});
 				}
 			}
 		}
